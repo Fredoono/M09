7435fac7a4b8abe31231c5697c8d13f9837c56f4
Fredoono <mimikyopokemon@gmail.com>
2024-09-25 12:26:02 +0100

    Add

---
+++ Classeveiculo/Classeveiculo/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classeveiculo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //criar um obejeto do tipo veículo
+            Veiculo carro0 = new Veiculo();
+            Console.WriteLine(carro0.calcular_velocidade(60, 1));
+            carro0.cilindrada = 1200;
+            carro0.marca = "opel";
+            carro0.modelo = "Corsa";
+
+            Veiculo carro1 = new Veiculo();
+            carro1 = new Veiculo();
+            carro1.cilindrada = 1900;
+            carro1.marca = "aqudi";
+            carro1.modelo = "a4";
+
+            carro0.visualizar_info();
+            carro1.visualizar_info();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Classeveiculo/Classeveiculo/Veículo.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classeveiculo
+{
+    internal class Veiculo
+    {
+        //caracteristicas do veículo
+        public string marca;
+        public string modelo;
+        public string cor;
+        public int ano_fabrica;
+        public string matricula;
+        public int potencia;
+        public int cilindrada;
+        public int n_portas;
+
+        //comportamento
+
+        public float calcular_velocidade(float dist, float temp)
+        {
+
+            return (dist / temp);
+        }
+
+        public void visualizar_info()
+        {
+            Console.WriteLine("o veiculo " + marca + " " + modelo + " possui a cilindrada " + cilindrada + "cc");
+
+        }
+
+    }
+}
